0e428f543bf5b22a6fb7ce761f44a274e1f3ab68
Juan Cruz Visino <visinojuancruz@gmail.com>
2020-08-06 23:51:46 -0300

    Todo listo

--- README.txt
+++ README.txt
@@ -4,6 +4,13 @@ Los casilleros negros multiplican los puntos de la letra x0,5
 Los casilleros amarillos multiplican los puntos de la letra x2
 
 
+Los niveles de dificultad  :
+Nivel 1 : Se pueden usar todo tipo de palabras
+Nivel 2 : Se pueden usar palabras que sean verbos y que esten en pattern.es.spelling.
+Nivel 3 : Solo verbos.
+
+Integrantes : Visino Juan Cruz
+
 
 Ejecutar en terminal:
-python3 bienvenida.py
+python3 ScrabbleAR.py
---
+++ ScrabbleAR.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg
+import configuracion as config
+import juego as game
+
+#background = '#000000'
+#sg.SetOptions(background_color=background, element_background_color=background)
+#sg.theme_previewer()
+sg.theme('Light Brown 9')
+layout = [[sg.Text("")],
+		  [sg.Text("")],
+		  [sg.Text(" "*15),sg.Button("",image_filename="./IconosFichas/bienvenida.png",disabled=False),sg.Text(" "*10)],
+		  [sg.Text("")],
+		  [sg.Text("")],
+		  [sg.Text("")],
+		  [sg.Text("")],
+		  [sg.Text("")],
+		  [sg.Text(" "*35),sg.Button("",image_filename="./IconosFichas/comenzar.png",disabled=False,key="_COMENZAR_"),sg.Text(" "*10)],
+		  [sg.Text("")],
+		  [sg.Text(""),sg.Button("",image_filename="./IconosFichas/configuracion.png",disabled=False,key="_CONFIG_"),sg.Text(" "*20)],
+		  [sg.Text("")],
+		  [sg.Text(" "*55),sg.Button("",image_filename="./IconosFichas/salir.png",disabled=False,key="_EXIT_"),sg.Text(" "*25)]]
+
+window = sg.Window("Scrable Python2020", layout,size =(800,600))
+
+program = True
+event,values = window.read()
+while program:
+	
+	
+	if event is "_COMENZAR_":
+		
+		game.iniciar()
+		window.close()
+		program = False
+	if event is "_CONFIG_":
+		
+		config.iniciar()
+	if event is "_EXIT_":
+		program= False
+		break
+	event,values = window.read()
+
+window.close()
